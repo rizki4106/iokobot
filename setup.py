c4a4ece1976c1e1050db003e805f9e4715f0589f
rizki4106 <rizkimaulana4106@gmail.com>
2021-02-03 07:31:52 +0700

    fix bug

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 setup(
   name = 'iokobot',
   packages = ['iokobot'],
-  version = '0.5',
+  version = '0.6',
   license='MIT',
   description = 'iokobot is a python library to help you make a smart bot, using machine learning algorithm',
   long_description=readme(),
